4a2a3a49c4bf0fae0e9c3122ddd9223e40d4bbde
A-Crofoot <77712351+A-Crofoot@users.noreply.github.com>
2021-01-19 20:36:48 -0800

    Add files via upload

---
+++ Lab_0.py
@@ -0,0 +1,14 @@
+
+################################################################
+#                                                              #
+# Austin W. Crofoot                                            #
+# ECE 351-53                                                   #
+# Due 1/26/2021                                                #
+#                                                              #
+################################################################
+
+import numpy
+import matplotlib.pyplot
+import scipy
+import control
+import pandas
